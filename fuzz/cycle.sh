d3108a63676913491bf2507aefef0149bee3c6e0
Rudraksh Joshi <u22ee054@eed.svnit.ac.in>
2026-03-17 01:04:04 +0530

    Add P2P network-message fuzz target and fix UserAgent arbitrary

--- fuzz/cycle.sh
+++ fuzz/cycle.sh
@@ -20,7 +20,6 @@ do
 
     # fuzz for one hour
     chrt -i 0 cargo +nightly fuzz run "$targetName" -- -max_total_time=3600
-    cargo +nightly fuzz cmin "$targetName"
+    cargo +nightly fuzz cmin "$targetName" 
   done
 done
-
